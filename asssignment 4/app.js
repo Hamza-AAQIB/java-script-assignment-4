98f2a235532f54d4e786096e51680ed8d6749ab7
Hamza-AAQIB <122180284+Hamza-AAQIB@users.noreply.github.com>
2023-02-09 07:40:58 -0800

    i upload java script assignment 4

---
+++ asssignment 4/app.js
@@ -0,0 +1,75 @@
+// pdf 9 t0 11
+// question no 1
+
+// var city =prompt("enter city name");
+// if(city == "karachi") {
+//     alert("welcome to city of lights")
+// }
+// question no 2
+// var gen =prompt("enter your gender");
+// if(gen == "male") {
+//     alert("good morning sir")
+// }
+// else{
+//     alert("good morning ma'am")
+// }
+
+// question no 3
+// var light1 =prompt("enter traffic light name");
+
+// if(light1 == "red"){
+//     alert("must stop")
+// }
+// else if(light1 == "yellow"){
+//     alert("ready tp move")
+// }
+// else if(light1 == "green"){
+//     alert("move now")
+// }
+
+// question no 4
+// var fuel =prompt("full your fuel tanl")
+// if(fuel < "0.25"){
+//     alert("plese fullfil your fuel tank")
+// }
+
+// question no 5
+// (a)
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// (b)
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// (c)
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// (d)
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// (e)
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+// question no 6
